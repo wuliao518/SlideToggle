2e0d0d1d1fcfb1c57c4b6fe102f109d7a4554286
wuliao518 <dazhengxie@gmail.com>
2014-08-10 22:19:52 +0800

    first commit

---
+++ SlidesToggle/src/com/example/slidetoggle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.slidetoggle;
+
+import com.example.slidetoggle.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main_activity);
+	}
+}
---
+++ SlidesToggle/src/com/example/slidetoggle/view/SlideToggle.java
@@ -0,0 +1,14 @@
+package com.example.slidetoggle.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class SlideToggle extends View {
+
+	public SlideToggle(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		// TODO Auto-generated constructor stub
+	}
+
+}
